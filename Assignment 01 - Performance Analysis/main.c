8769dfb42d5f2feb996fa72bef09d166ba377f7a
Channel <69780324+channel104257@users.noreply.github.com>
2022-03-14 18:41:47 +0800

    Update main.c

--- Assignment 01 - Performance Analysis/main.c
+++ Assignment 01 - Performance Analysis/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <time.h> // for clock_t, clock(), CLOCKS_PER_SEC 
+#include "unistd.h" // for sleep()
 
 int ackerman(int m, int n);
 
 int main()
 {
+	// to store the execution time of code 
+	double time_spent = 0.0;
+	clock_t t_begin = clock();
+
 	// input variable m & n
 	int m, n; 
 
@@ -11,7 +17,16 @@ int main()
 	scanf_s("%d %d", &m, &n); 
 
 	// output the final answer
-	printf("%d", ackerman(m, n)); 
+	printf("%d\n", ackerman(m, n)); 
+
+	clock_t t_end = clock();
+	printf("\n");
+
+	// calculate elapsed time by finding difference (end - begin) and  
+	// dividing the difference by CLOCKS_PER_SEC to convert to seconds  
+	time_spent += (double)(t_end - t_begin) / CLOCKS_PER_SEC; 
+
+	printf("The elapsed time is %f seconds", time_spent);
 }
 
 int ackerman(int m, int n)

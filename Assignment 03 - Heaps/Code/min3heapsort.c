7596438e095c4bc28457f00d4898ea1c0fe22f78
Channel <69780324+channel104257@users.noreply.github.com>
2022-06-13 21:59:15 +0800

    Update min3heapsort.c

--- Assignment 03 - Heaps/Code/min3heapsort.c
+++ Assignment 03 - Heaps/Code/min3heapsort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h> //for timeing
 
+// 定義節點結構
 typedef struct HeapNode
 {
     int data;
@@ -14,40 +16,52 @@ typedef struct HeapNode *heapPtr;
 void insertlist(heapPtr *nodePtr, int info);
 // 顯示串列的所有元素
 void printlist(heapPtr currentPtr);
-//
+// 計算當前節點數量
 int nodecount(heapPtr currentPtr);
-//
+// 移動堆積指標位置
 heapPtr MoveToNode(heapPtr nodePtr, int num);
-//
+// 交換
 void swap(int *num1, int *num2);
-//
+// 最小堆積比較
 void MinHeapComparison(heapPtr headPtr, int size, int No);
-//
+// 堆積排序法
 void HeapSort(heapPtr headPtr, int size);
 
+// 主程式進入點
 int main()
 {
+    // 定義輸入字元
     char value;
+    // 定義字元數量旗標
     int tempNum = 0;
+    // 定義首位堆積節點
     heapPtr heapNodeHead = NULL;
+    // set up the variable for timing
+    LARGE_INTEGER t1, t2, tc;
 
     printf("請輸入 Heap 的 Level-Order Traversal\n");
     printf("(請以「空格」區隔各個node)\n-> ");
 
+    // 逐字讀取輸入字元直到'\n'
     while ((value = getchar()) != '\n')
     {
         switch (value)
         {
+        // 以空白間隔表輸入完畢
         case ' ':
         {
+            // 將該數字輸入初始堆積
             insertlist(&heapNodeHead, tempNum);
+            // 重製字元數量旗標
             tempNum = 0;
             break;
         }
         default:
         {
+            // 若輸入為數字
             if (value >= 48 && value <= 57)
             {
+                // 將已讀取的數字累加
                 tempNum = tempNum * 10 + (value - 48);
             }
             break;
@@ -55,24 +69,46 @@ int main()
         }
     }
 
+    // 將最後的數字推入初始堆積
     insertlist(&heapNodeHead, tempNum);
 
+    // 輸出初始堆積
     printlist(heapNodeHead);
 
+    // 逐節點訪問進行 Buttom-up
     for (int i = nodecount(heapNodeHead); i > 0; i--)
     {
         MinHeapComparison(heapNodeHead, nodecount(heapNodeHead), i);
     }
 
+    // 輸出最小堆積
     printf("\n");
-    printf("Minimum 3-Heap\n");
+    printf("Min 3-Heap\n");
     printlist(heapNodeHead);
+
+    // check the tick frequency
+    QueryPerformanceFrequency(&tc);
+
+    // start timing
+    QueryPerformanceCounter(&t1);
 
     HeapSort(heapNodeHead, nodecount(heapNodeHead));
+    
+    // stop timing
+    QueryPerformanceCounter(&t2);
+
+    // calculate elapsed time by finding difference (end - begin) and
+    // dividing the difference by CLOCKS_PER_SEC to convert to seconds
+    double time = ((double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart) * 1000;
+
     printf("After HeapSort\n");
     printlist(heapNodeHead);
+    // 歷經時間
+    printf("The elapsed time is %lf ms\n", time);
+    printf("\n");
 }
 
+// 插入節點
 void insertlist(heapPtr *headPtr, int info)
 {
     // 建立新的節點
@@ -153,6 +189,7 @@ int nodecount(heapPtr currentPtr)
     }
     else
     {
+        // 定義節點數量
         int nodeNum = 0;
 
         // 將當前串列位置往後移直到到達尾端
@@ -160,6 +197,7 @@ int nodecount(heapPtr currentPtr)
         {
             // 將當前串列位置往後移
             currentPtr = currentPtr->nextPtr;
+            // 節點數量 +1 
             nodeNum++;
         }
 
@@ -178,6 +216,7 @@ heapPtr MoveToNode(heapPtr nodePtr, int num)
     return nodePtr;
 }
 
+// 交換
 void swap(int *num1, int *num2)
 {
     int temp = *num1;
@@ -187,37 +226,52 @@ void swap(int *num1, int *num2)
 
 void MinHeapComparison(heapPtr headPtr, int size, int No)
 {
+    //定義最小節點
     int smallest = No;
+    // 定義 left child
     int left = 3 * No - 1;
+    // 定義 medium child
     int medium = 3 * No;
+    // 定義 right child
     int right = 3 * No + 1;
 
-    // If left child is smaller than root
+    // 若 left child 小於最小節點的值
     if (left <= size && MoveToNode(headPtr, left)->data < MoveToNode(headPtr, smallest)->data)
     {
+        // 最小節點為 left child
         smallest = left;
     }
+    // 若 medium child 小於最小節點的值
     if (medium <= size && MoveToNode(headPtr, medium)->data < MoveToNode(headPtr, smallest)->data)
     {
+        // 最小節點為 medium child
         smallest = medium;
     }
+    // 若 right child 小於最小節點的值
     if (right <= size && MoveToNode(headPtr, right)->data < MoveToNode(headPtr, smallest)->data)
     {
+        // 最小節點為 right child
         smallest = right;
     }
+    // 若最小節點不為 root
     if (smallest != No)
     {
+        // 交換 root & 最小節點
         swap(&MoveToNode(headPtr, No)->data, &MoveToNode(headPtr, smallest)->data);
+        // 向下層子樹遞迴
         MinHeapComparison(headPtr, size, smallest);
     }
 }
 
+// 堆積排序法
 void HeapSort(heapPtr headPtr, int size)
 {
     for (size_t i = 1; i <= size; i++)
     {
+        // 將極值與 root 值交換
         swap(&MoveToNode(headPtr, 1)->data, &MoveToNode(headPtr, size - i + 1)->data);
 
+        // 縮減區間進行heapify
         MinHeapComparison(headPtr, size - i, 1);
     }
 }
